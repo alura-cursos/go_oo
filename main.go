929cafecc07a9827699735a5a9209881bed23106
Guilherme Lima <guilhermelimaruby@gmail.com>
2019-08-26 14:05:53 -0300

    aula 2

--- main.go
+++ main.go
@@ -9,12 +9,24 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	fmt.Println(contaDaSilvia.saldo)
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
+	fmt.Println(contaDaSilvia.Sacar(400))
+	fmt.Println(contaDaSilvia.saldo)
+
 }
